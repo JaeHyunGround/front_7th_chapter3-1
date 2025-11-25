1f520d629283aa781389bb73a3de24a233e6532f
JaeHyunGround <gothddlek@naver.com>
2025-11-25 23:32:04 +0900

    refactor : common 폴더 추가
    - 공통 컴포넌트를 담아두기 위해 common 폴더를 생성했습니다.

--- packages/after/src/components/ui/common/button.tsx
+++ packages/after/src/components/ui/common/button.tsx
@@ -9,12 +9,14 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
+        none: "border-none bg-transparent",
         blue: "bg-blue-200 text-white border-blue-300 hover:bg-blue-300 font-bold",
         gray: "bg-gray-100 text-gray-500 border-gray-300 hover:bg-gray-200 font-normal",
         red: "bg-red-100 text-white border-red-200 hover:bg-red-200",
         green: "bg-green-100 text-white border-green-200 hover:bg-green-200",
       },
       size: {
+        none: "",
         sm: "py-[6px] px-[12px] text-[13px]",
         md: "py-[10px] px-[20px] text-[14px]",
         lg: "py-[12px] px-[24px] text-[15px]",
