ba5ffd0725102625fc75a86c68c30d6e4900fbb4
JaeHyunGround <gothddlek@naver.com>
2025-11-27 02:19:07 +0900

    chore : 배포 세팅

--- packages/after/vite.config.ts
+++ packages/after/vite.config.ts
@@ -3,12 +3,9 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
-const base: string =
-  process.env.NODE_ENV === "production" ? "/front_7th_chapter3-1/" : "";
-
 // https://vite.dev/config/
 export default defineConfig({
-  base,
+  base: "/front_7th_chapter3-1/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
